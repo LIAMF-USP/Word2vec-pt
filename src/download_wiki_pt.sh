9d29cd0d89806a567efec133584ea67a9aba0c30
felipessalvatore <felipessalvador@egooglemail.com>
2017-03-21 19:10:45 -0300

    Add script for download data

---
+++ src/download_wiki_pt.sh
@@ -0,0 +1,10 @@
+wget https://www.dropbox.com/s/pjluetvwgl6k5ah/pt96.txt.zip?dl=0
+wget https://www.dropbox.com/s/qn4uop3dvl960tf/Wiki.zip?dl=0 
+unzip Wiki.zip?dl=0
+unzip pt96.txt.zip?dl=0
+mv Wiki.txt data/Wiki.txt
+mv pt96.txt data/pt96.txt
+rm Wiki.zip\?dl\=0 
+rm pt96.txt.zip?dl=0
+
+
