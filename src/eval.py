0c777efeeea16894d0b3c934e14b2814c3dd1677
Felsal <felipessalvador@googlemail.com>
2017-03-28 18:49:50 -0300

    Update and rename eval_pt.py to eval.py

--- src/eval.py
+++ src/eval.py
@@ -11,6 +11,13 @@ parser.add_argument("-f",
                     help="""pickle file to apply
                     the evaluation function (default=None)""")
 
+parser.add_argument("-l",
+                    "--language",
+                    type=str,
+                    default='pt',
+                    help="""language selection: pt, en (default=pt)""")
+
+
 args = parser.parse_args()
 file_path = args.file
 with open(file_path, "rb") as s:
@@ -22,7 +29,10 @@ embeddings = d['embeddings']
 word2index = d['word2index']
 index2word = d['index2word']
 
-eval_path = "./evaluation/questions-words-ptbr.txt"
+if args.language == 'pt':
+  eval_path = "./evaluation/questions-words-ptbr.txt"
+else:
+  eval_path = "./evaluation/questions-words.txt"
 score, report = util.score(index2word,
                            word2index,
                            embeddings,
